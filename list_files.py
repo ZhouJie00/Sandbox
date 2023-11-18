889eb5cf24a14b7cc04f4b941dfaef1efb945601
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2023-11-18 14:56:04 +0800

    Third commit

---
+++ list_files.py
@@ -0,0 +1,9 @@
+#this is list_files
+
+import os
+
+print(f"The files and folders in {os.getcwd()} are:")
+items = os.listdir('.')
+for item in items:
+    prefix = "(d) " if os.path.isdir(item) else "(f) "
+    print(f"{prefix}\t{item}")
